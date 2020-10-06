0c15ffc014e34552580ee3f12d9bbf3b2df9a05c
fullstackdev2020 <fullstackdeveloper2020@outlook.com>
2020-10-06 17:23:32 +0500

    ProductController with NUnitTests

---
+++ Aphesia.EF/Migrations/202010051335080_Aphesia.cs
@@ -0,0 +1,20 @@
+﻿namespace Aphesia.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Aphesia : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Description", c => c.String());
+            DropColumn("dbo.Products", "Descrption");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "Descrption", c => c.String());
+            DropColumn("dbo.Products", "Description");
+        }
+    }
+}
